7ed831fe6e12dea4ea59d325a003f08b7ecacbdd
Aditya0215 <adityaravan8055@gmail.com>
2023-07-12 12:06:59 +0530

    this is for practise purpose

---
+++ Variables.py
@@ -0,0 +1,29 @@
+a=10
+print (a)
+a=10
+b=20
+print(a,b)
+
+## String concat
+a= "HELLO"
+b= "ADITYA"
+print(a+" "+b)
+
+## OPOERATORS
+#ARTHEMATIC
+c=10
+d=20
+print(c+d)
+print(c-d)
+print(c*d)
+print(c/d)
+print(c**2)
+print(c%d)
+print(c//2)
+#ASSIGNMENT
+x=5
+print(x)
+x=x+5
+print(x)
+
+
